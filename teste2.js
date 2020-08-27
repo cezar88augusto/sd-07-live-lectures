52a82f9660cd00a099a1270bf3215470237543fa
LucasCFerraz <lucascfpaolillo@gmail.com>
2020-08-27 15:49:26 -0300

    Initial commit

---
+++ teste2.js
@@ -0,0 +1,8 @@
+/* 
+   Hora de fazermos juntos!
+   Agora vamos ter 10 minutos para construir um algoritmo com a seguinte descrição:
+   Crie dois valores num1 e num2.
+   Se os valores forem iguais some os dois
+   Caso contrário multiplique num1 por num2.
+   Por ultimo exiba o valor final no console.
+*/
