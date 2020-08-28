01683b8ef2ac417b0e59dc7ed2fe180ff2984ff3
LucasCFerraz <lucascfpaolillo@gmail.com>
2020-08-28 16:58:51 -0300

    Algoritmo de validação da mega-sena para uma pessoa

--- megasena.js
+++ megasena.js
@@ -1,4 +1,4 @@
-// Ter um array com os números da mega sena
+// Ter um array com os números da mega sena sorteados de forma aleatória
 let megaSenaNumbers = [];
 
 let firstNumber = Math.ceil(Math.random() * 60);
@@ -10,5 +10,20 @@ let sixthNumber = Math.ceil(Math.random() * 60);
 
 megaSenaNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
 
+let turma7Play = [7, 16, 28, 31, 37, 55];
 
-console.log(megaSenaNumbers);
+let numberOfHits = 0;
+
+for (let i = 0; i < turma7Play.length; i += 1) {
+  let playedNumber = turma7Play[i];
+  for (let j = 0; j < megaSenaNumbers.length; j += 1) {
+    let megaSenaNumber = megaSenaNumbers[j];
+    if(playedNumber === megaSenaNumber) {
+      numberOfHits += 1;
+    }
+  }
+}
+
+console.log(`Números sorteados: ${megaSenaNumbers}`);
+console.log(`Nossas tentativas: ${turma7Play}`);
+console.log(`Total de acertos: ${numberOfHits}`);
