c627cd73a8f787bfab634b307ca14d2aa0f1d2be
LucasCFerraz <lucascfpaolillo@gmail.com>
2020-08-28 17:20:06 -0300

    Validando vários jogos da mega-sena

--- megasena.js
+++ megasena.js
@@ -11,19 +11,29 @@ let sixthNumber = Math.ceil(Math.random() * 60);
 megaSenaNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
 
 let turma7Play = [7, 16, 28, 31, 37, 55];
+let turma72Play = [7, 16, 28, 31, 38, 55];
+let turma73Play = [7, 17, 28, 31, 37, 55];
 
-let numberOfHits = 0;
+let plays = [turma7Play, turma72Play, turma73Play];
 
-for (let i = 0; i < turma7Play.length; i += 1) {
-  let playedNumber = turma7Play[i];
-  for (let j = 0; j < megaSenaNumbers.length; j += 1) {
-    let megaSenaNumber = megaSenaNumbers[j];
-    if(playedNumber === megaSenaNumber) {
-      numberOfHits += 1;
+// Testa os 3 jogos
+for(let indice = 0; indice < plays.length; indice += 1) {
+  let numberOfHits = 0;
+  let currentPlay = plays[indice];
+  // Compara os jogos
+  for (let i = 0; i < currentPlay.length; i += 1) {
+    let playedNumber = currentPlay[i];
+    for (let j = 0; j < megaSenaNumbers.length; j += 1) {
+      let megaSenaNumber = megaSenaNumbers[j];
+      if(playedNumber === megaSenaNumber) {
+        numberOfHits += 1;
+      }
     }
   }
+
+  console.log(`Números sorteados: ${megaSenaNumbers}`);
+  console.log(`Nossas tentativas: ${currentPlay}`);
+  console.log(`Total de acertos: ${numberOfHits}`);
 }
 
-console.log(`Números sorteados: ${megaSenaNumbers}`);
-console.log(`Nossas tentativas: ${turma7Play}`);
-console.log(`Total de acertos: ${numberOfHits}`);
+
