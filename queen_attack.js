388fb9dc2187bbc5ae39068856fe140efd03d784
LucasCFerraz <lucascfpaolillo@gmail.com>
2020-08-31 17:44:39 -0300

    Verificando as outras diagonais

--- queen_attack.js
+++ queen_attack.js
@@ -4,7 +4,7 @@ let posicaoRainhaColuna = 4;
 
 // Guarda a posição da Peça
 let posicaoPecaLinha = 7;
-let posicaoPecaColuna = 7;
+let posicaoPecaColuna = 1;
 
 let ataqueBemSucedido = false;
 
@@ -19,6 +19,51 @@ for (let i = 1; i < 8; i += 1) {
   let colunaRainha = posicaoRainhaColuna + i;
 
   if (linhaRainha > 8 || colunaRainha > 8) {
+    break;
+  }
+
+  if (linhaRainha === posicaoPecaLinha && colunaRainha === posicaoPecaColuna) {
+    ataqueBemSucedido = true;
+  }
+}
+
+// Diagonal superior esquerda
+// 4 x 4
+for (let i = 1; i < 8; i += 1) {
+  let linhaRainha = posicaoRainhaLinha + i;
+  let colunaRainha = posicaoRainhaColuna - i;
+
+  if (linhaRainha > 8 || colunaRainha < 1) {
+    break;
+  }
+
+  if (linhaRainha === posicaoPecaLinha && colunaRainha === posicaoPecaColuna) {
+    ataqueBemSucedido = true;
+  }
+}
+
+// Diagonal inferior esquerda
+// 4 x 4
+for (let i = 1; i < 8; i += 1) {
+  let linhaRainha = posicaoRainhaLinha - i;
+  let colunaRainha = posicaoRainhaColuna - i;
+
+  if (linhaRainha < 1 || colunaRainha < 1) {
+    break;
+  }
+
+  if (linhaRainha === posicaoPecaLinha && colunaRainha === posicaoPecaColuna) {
+    ataqueBemSucedido = true;
+  }
+}
+
+// Diagonal inferior direita
+// 4 x 4
+for (let i = 1; i < 8; i += 1) {
+  let linhaRainha = posicaoRainhaLinha - i;
+  let colunaRainha = posicaoRainhaColuna + i;
+
+  if (linhaRainha < 1 || colunaRainha > 8) {
     break;
   }
 
