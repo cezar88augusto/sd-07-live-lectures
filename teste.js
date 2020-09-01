5919973caaafe8f63066d6f9415788ba229ad5b4
LucasCFerraz <lucascfpaolillo@gmail.com>
2020-09-01 17:20:54 -0300

    Função soma

--- teste.js
+++ teste.js
@@ -12,13 +12,31 @@ estudante["esporte"] = "Corrida";
 estudante["nome"] = "João";
 
 estudante.apelidos = ["Apelido1", "Apelido2", "Apelido3"];
-estudante.endereço = {
+estudante.endereco = {
   rua: "Rua sei la o que",
   numero: 401
 }
 
+console.log(estudante);
+
 for (let key in estudante) {
   console.log(estudante[key]);
 }
+
+// Função trybe
+
+// Soma
+
+function soma(a, b) {
+  return a + b;
+}
+
+let valor1 = 5;
+let valor2 = 10;
+
+let valorSomado = soma(valor1, valor2);
+
+console.log(valorSomado);
+
 
 // console.log(estudante);
