3692506941b8874bcf29a0efe2ee80be2e6764f1
LucasCFerraz <lucascfpaolillo@gmail.com>
2020-09-01 17:56:05 -0300

    Refatoração Mega Sena

--- megasena.js
+++ megasena.js
@@ -1,14 +1,17 @@
 // Ter um array com os números da mega sena sorteados de forma aleatória
-let megaSenaNumbers = [];
+function getRandomNumberBetween1AndX(maxValue) {
+  return Math.ceil(Math.random() * maxValue);
+}
 
-let firstNumber = Math.ceil(Math.random() * 60);
-let secondNumber = Math.ceil(Math.random() * 60);
-let thirdNumber = Math.ceil(Math.random() * 60);
-let fourthNumber = Math.ceil(Math.random() * 60);
-let fifthNumber = Math.ceil(Math.random() * 60);
-let sixthNumber = Math.ceil(Math.random() * 60);
+function generateMegaSenaNumbers() {
+  let megaSenaNumbers = [];
+  for (let i = 0; i < 6; i += 1) {
+    megaSenaNumbers.push(getRandomNumberBetween1AndX(60));
+  }
+  return megaSenaNumbers;
+}
 
-megaSenaNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
+let megaSenaNumbers = generateMegaSenaNumbers();
 
 let turma7Play = [7, 16, 28, 31, 37, 55];
 let turma72Play = [7, 16, 28, 31, 38, 55];
@@ -16,20 +19,27 @@ let turma73Play = [7, 17, 28, 31, 37, 55];
 
 let plays = [turma7Play, turma72Play, turma73Play];
 
-// Testa os 3 jogos
-for(let indice = 0; indice < plays.length; indice += 1) {
+function getPlayHits(msNumbers, play) {
   let numberOfHits = 0;
-  let currentPlay = plays[indice];
-  // Compara os jogos
-  for (let i = 0; i < currentPlay.length; i += 1) {
-    let playedNumber = currentPlay[i];
-    for (let j = 0; j < megaSenaNumbers.length; j += 1) {
-      let megaSenaNumber = megaSenaNumbers[j];
+
+  for (let i = 0; i < play.length; i += 1) {
+    let playedNumber = play[i];
+    for (let j = 0; j < msNumbers.length; j += 1) {
+      let megaSenaNumber = msNumbers[j];
       if(playedNumber === megaSenaNumber) {
         numberOfHits += 1;
       }
     }
   }
+
+  return numberOfHits;
+}
+
+// Testa os 3 jogos
+for(let indice = 0; indice < plays.length; indice += 1) {
+  let currentPlay = plays[indice];
+  
+  let numberOfHits = getPlayHits(megaSenaNumbers, currentPlay);
 
   console.log(`Números sorteados: ${megaSenaNumbers}`);
   console.log(`Nossas tentativas: ${currentPlay}`);
