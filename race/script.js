e0c3b9dc290ec589d0d96b8357b19917ce61de5e
Gabriel Oliva <gfpoliva@gmail.com>
2020-09-09 18:05:28 -0300

    Finalizado projeto

--- race/script.js
+++ race/script.js
@@ -9,7 +9,21 @@ window.onload = function() {
   button.addEventListener('click', function() {
     car1.style.marginLeft = parseInt(car1.style.marginLeft) + (Math.random() * 100) + "px";
     car2.style.marginLeft = parseInt(car2.style.marginLeft) + (Math.random() * 100) + "px";
-  });
 
+    if (parseInt(car1.style.marginLeft) > window.innerWidth) {
+      alert("carro 1 ganhou!");
+      reset(car1, car2);
+    }
+
+    if (parseInt(car2.style.marginLeft) > window.innerWidth) {
+      alert("carro 2 ganhou!");
+      reset(car1, car2);
+    }
+
+  });
 }
 
+function reset(car1, car2) {
+  car1.style.marginLeft = "0px";
+  car2.style.marginLeft = "0px";
+}
