4c876f07bc17ed2295441224daf193db5d1aa8ad
Gabriel Oliva <gfpoliva@gmail.com>
2020-09-29 18:17:57 -0300

    Aula 7.3

--- summation.js
+++ summation.js
@@ -10,8 +10,26 @@
 
 const assert = require("assert");
 
-const summationOf = () => {
+const summationOf = (number) => {
+  if (typeof(number) !== "number") {
+    throw Error("Essa função só funciona com números!");
+  }
 
+  let summation = 0;
+
+  for (let index = 1; index <= number; index += 1) {
+    summation += index;
+  }
+
+  return summation;
 }
 
-assert.strictEqual(typeof(summationOf), "function");
+assert.strictEqual(typeof(summationOf), "function", "Deu ruim. O resultado esperado era function");
+assert.strictEqual(summationOf(1), 1);
+assert.strictEqual(summationOf(3), 6);
+assert.strictEqual(summationOf(5), 15);
+
+assert.throws(() => {
+  // algum codigo que deve levantar exceção. Ou seja, que deve dar erro
+  summationOf("3");
+});
