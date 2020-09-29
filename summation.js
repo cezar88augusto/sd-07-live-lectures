8bd1ba306d5240fb2b4f20be6dfb543d8d497599
Gabriel Oliva <gfpoliva@gmail.com>
2020-09-29 17:04:19 -0300

    teste que verifica se a funcao existe

---
+++ summation.js
@@ -0,0 +1,17 @@
+// Implemente uma função `summationOf(number)` que recebe um número inteiro
+// e retorna o seu somatório.
+
+// Por exemplo:
+// - `summationOf(1)` retorna 1: de 1 até 1 = 1
+// - `summationOf(3)` retorna 6: de 1 até 3 = 1 + 2 + 3 = 6
+// - `summationOf(5)` retorna 15: de 1 até 5 = 1 + 2 + 3 + 4 + 5 = 15
+
+// TDD
+
+const assert = require("assert");
+
+const summationOf = () => {
+
+}
+
+assert.strictEqual(typeof(summationOf), "function");
