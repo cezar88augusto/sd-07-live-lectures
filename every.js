64f4b8869948e49dde3d11263e7ba6111bf856ac
Gabriel Oliva <gfpoliva@gmail.com>
2020-10-05 17:36:26 -0300

    Exemplos do every

---
+++ every.js
@@ -0,0 +1,13 @@
+const users = [
+  { firstName: 'Homer', lastName: 'Simpson', isDriver: true },
+  { firstName: 'Marge', lastName: 'Simpson', isDriver: true },
+  { firstName: 'Bart', lastName: 'Simpson', isDriver: false },
+  { firstName: 'Lisa', lastName: 'Simpson', isDriver: false },
+  { firstName: 'Maggie', lastName: 'Simpson', isDriver: false }
+];
+
+const everyPersonCanDrive = users.every((user) => {
+  return user.isDriver === true;
+});
+
+console.log(everyPersonCanDrive);
