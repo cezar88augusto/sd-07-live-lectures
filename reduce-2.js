4f1747934df2ec25b67f35fb253f19de2f3f9d16
Gabriel Oliva <gfpoliva@gmail.com>
2020-10-07 17:29:25 -0300

    reduce examplo 2

--- reduce-2.js
+++ reduce-2.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
 
-const epicPhrase = epic.reduce();
+const epicPhrase = epic.reduce((accumulator, currentValue) => `${accumulator} ${currentValue}` );
 
 // Testes
 
