4f57811a60e339670acc0beb48b4bb5ac04b0d08
Gabriel Oliva <gfpoliva@gmail.com>
2020-10-07 17:27:13 -0300

    reduce 1

--- reduce-1.js
+++ reduce-1.js
@@ -9,9 +9,32 @@ for (let index = 1; index < numbers.length; index += 1) {
 }
 
 // Somando os números do array com reduce
-// const sumWithReduce = numbers.reduce();
+// 1. Reduce é uma função de alta ordem => HoF
+// 2. Ele recebe como primeiro parametro, uma callback
+// 3. O valor de previousValue será sempre o que for retornado anteriormente
+// 3.1. A primeira vez que a função callback é executada, o previousValue assumo o valor do primeiro elemento do array
+// 4. O currentValue tras o valor corrente de cada iteração do array
+const sumWithReduce = numbers.reduce((previousValue, currentValue) => previousValue + currentValue);
+
+// 1 iteração: previousValue === 2 | currentValue === 3
+// 2 iteração: previousValue === 5 | currentValue === 4
+// 3 iteração: previousValue === 9 | currentValue === 6
+// 4 iteração: previousValue === 15 | currentValue === 8
+// 5 iteração: previousValue === 23 | currentValue === 12
+// 6 iteração: previousValue === 35 | currentValue === 24
+// Retorno => 59
+
+console.log(`Valor com for: ${sumWithFor}`);
+console.log(`Valor com reduce: ${sumWithReduce}`);
 
 // Testes
 const expectedValue = 59;
 assert.strictEqual(sumWithFor, expectedValue);
-// assert.strictEqual(sumWithReduce, expectedValue);
+assert.strictEqual(sumWithReduce, expectedValue);
+
+// Some apenas os numeros pares do array
+const sumEvenNumbersWithReduce = numbers
+  .filter((number) => number % 2 === 0 )
+  .reduce((previousValue, currentValue) => previousValue + currentValue);
+
+  console.log(sumEvenNumbersWithReduce);
