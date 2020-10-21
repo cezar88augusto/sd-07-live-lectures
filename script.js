49b707c3dbd2653ab301cd210b4f9b213f6c7d40
Gabriel Oliva <gfpoliva@gmail.com>
2020-10-21 18:23:29 -0300

    Aula 9.1

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const fetchJoke = () => {
-  
-}
+const appendJoke = (joke) => {
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerHTML = joke;
+  ul.appendChild(li);
+};
 
-const requestJoke = (callback) => {
-  setTimeout(() => {
-    callback("Antes Deus e eu sabia o que esse código fazia. Agora só Deus sabe!");
-  }, 3000);
+const fetchJoke = () => {
+  const param = { headers: { Accept: 'application/json' } };
+  
+  fetch("https://icanhazdadjoke.com/search?term=spider", param)
+    .then((response) => {
+      response.json().then((data) => {
+        const joke = data.results[0].joke 
+        appendJoke(joke);
+      })
+    })
 }
 
 window.onload = fetchJoke();
