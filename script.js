342557f63d9c47d5e91470150732741b65887601
Gabriel Oliva <gfpoliva@gmail.com>
2020-10-22 18:11:13 -0300

    Aula 9.2

--- script.js
+++ script.js
@@ -5,16 +5,64 @@ const appendJoke = (joke) => {
   ul.appendChild(li);
 };
 
-const fetchJoke = () => {
+const getRandomJokeCallback = (term, callback) => {
   const param = { headers: { Accept: 'application/json' } };
-  
-  fetch("https://icanhazdadjoke.com/search?term=spider", param)
+  fetch(`https://icanhazdadjoke.com/search?term=${term}`, param)
     .then((response) => {
       response.json().then((data) => {
-        const joke = data.results[0].joke 
+        const joke = data.results[0].joke;
         appendJoke(joke);
+        callback ? callback() : undefined;
       })
     })
+}
+
+const fetchJokeCallback = () => {
+  getRandomJoke("spider", 
+    getRandomJoke("pizza",
+      getRandomJoke("wolf",
+        getRandomJoke("ant")
+      )
+    )
+  );
+}
+
+const getRandomJoke = (term) => {
+  return new Promise((resolve, reject) => {
+    const param = { headers: { Accept: 'application/json' } };
+    fetch(`https://icanhazdadjoke.com/search?term=${term}`, param)
+      .then((response) => {
+        response.json()
+          .then((data) => {
+            const joke = data.results[0].joke;
+            appendJoke(joke);
+            resolve();
+          })
+      });
+  });
+}
+
+// const fetchJoke = () => {
+//   getRandomJoke("spider")
+//     .then(() => getRandomJoke("pizza"))
+//     .then(() => getRandomJoke("ant"))
+//     .then(() => getRandomJoke("wolf"))
+//     .catch((error) => console.log(error));
+
+//   console.log("XABLAU");
+// }
+
+const fetchJoke = async () => {
+  try {
+    await getRandomJoke("spider");
+    await getRandomJoke("pizza");
+    await getRandomJoke("wolf");
+    await getRandomJoke("ant");
+
+    console.log("XABLAU");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 window.onload = fetchJoke();
