adb3fff8a5347146fb150335360c4c5576ed7a43
Mauricio Homma <mhhomma@gmail.com>
2020-11-05 17:35:31 -0300

    lecture code

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './HelloWorld';
+
+class App extends React.Component {
+  render() {
+    return (<HelloWorld />);
+  }
+}
+
+export default App;
---
+++ my-app/src/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  render() {
+    return(<span>Hello, World! De um componente</span>);
+  }
+}
+
+export default HelloWorld;
